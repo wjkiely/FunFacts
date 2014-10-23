fdb467d6363c448ac8d9c3b2e8d190824015fd17
William Kiely <wjkiely@gmail.com>
2014-10-23 08:47:30 -0300

    Fun Facts App
    Completed Fun Facts app from Treehouse Android Development tutorial
    "Build a Simple Android App"

---
+++ app/src/main/java/com/williamkiely/funfacts/ColorWheel.java
@@ -0,0 +1,44 @@
+package com.williamkiely.funfacts;
+
+import android.graphics.Color;
+
+import java.util.Random;
+
+/**
+ * Created by William on 10/22/2014.
+ */
+public class ColorWheel {
+
+    // Member variable (properties about the object)
+    public String[] mColor = {
+            "#39add1", // light blue
+            "#3079ab", // dark blue
+            "#c25975", // mauve
+            "#e15258", // red
+            "#f9845b", // orange
+            "#838cc7", // lavender
+            "#7d669e", // purple
+            "#53bbb4", // aqua
+            "#51b46d", // green
+            "#e0ab18", // mustard
+            "#637a91", // dark gray
+            "#f092b0", // pink
+            "#b7c0c7"  // light gray
+    };
+
+    // Method (abilities: things the object can do)
+    public int getColor() {
+
+        String color = "";
+
+        // Randomly select a fact
+        Random randomGenerator = new Random(); // Construct a new Random number generator
+        int randomNumber = randomGenerator.nextInt(mColor.length);
+
+        color = mColor[randomNumber];
+        int colorAsInt = Color.parseColor(color);
+
+        return colorAsInt;
+    }
+}
+
---
+++ app/src/main/java/com/williamkiely/funfacts/FunFactsActivity.java
@@ -0,0 +1,54 @@
+package com.williamkiely.funfacts;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class FunFactsActivity extends Activity {
+
+    public static final String TAG = FunFactsActivity.class.getSimpleName();
+
+    private FactBook mFactBook = new FactBook();
+    private ColorWheel mColorWheel = new ColorWheel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Declare our View variables and assign them the Views from the layout file
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        final Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fact = mFactBook.getFact();
+                // Update the label with our dynamic fact
+                factLabel.setText(fact);
+
+                int color = mColorWheel.getColor();
+                relativeLayout.setBackgroundColor(color);
+                showFactButton.setTextColor(color);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
+
+        // Toast.makeText(this, "Yay! Our Activity was created!", Toast.LENGTH_LONG).show();
+        Log.d(TAG, "We're logging from the onCreate() method!");
+    }
+}
+
+
+
+
+
+
